acbc5c58e474d877264369cb267994e037c79590
rewan1312 <rewanelwardany99@gmail.com>
2025-03-21 01:35:25 +0200

    server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors()); // ✅ تمكين CORS
+app.use(cors());
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
